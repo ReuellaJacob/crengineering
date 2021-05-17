559e61f42fcc924221977fc0ab88f380e19bae95
ReuellaJacob <reuella.jacob@calbaptist.edu>
2021-05-17 05:02:05 -0700

    Reverted back to before connecting to the API

--- src/App.js
+++ src/App.js
@@ -3,46 +3,34 @@ import CommunityRequest from './components/CommunityRequest';
 import Modal from "./components/ModalViewForm";
 import React from "react";
 import {useState} from "react";
+import CommunityRequestForm from "./components/CommunityRequestForm";
 
-class App extends React.Component {
-    state = {
-        isLoading: true,
-        communityRequests: []
-    };
-
-    async componentDidMount() {
-        const response = await fetch('/requests');
-        const body = await response.json;
-        this.setState({communityRequests: body, isLoading: false});
-    }
-
-    render() {
+function App() {
         let introductionText = "Welcome to the community request page. We are so glad to see you here. " +
             "This page is a platform for people to share what they are struggling with. " +
             "You can be as specific as you would like or as anonymous as you want to be. " +
             "We only ask that you close out your requests when you can. We hope this platform " +
             "gives you a safe space to share your needs.";
-        const {communityRequests, isLoading} = this.state;
-
-        if (isLoading) {
-            return <p>Loading...</p>
-        }
+        const [show, setShow] = useState(false);
 
         return (
             <div className="App">
                 <h1 className='textOnMainPage' id='websiteName'>Community Helper</h1>
                 <p className='textOnMainPage' id='introduction'>{introductionText}</p>
-                <button className='buttons' id='newRequestButton'>New Request</button>
-                <Modal/>
-                {communityRequests.map(communityRequests =>
-                    <div className="requestCards">
-                        <CommunityRequest cardTitle={"CommunityRequests"} cardBody={communityRequests.content}
-                                          cardDate={communityRequests.entryDate}/>
-                    </div>
-                )}
+                <button className='buttons' id='newRequestButton' onClick={() => setShow(true)}>New Request</button>
+                <Modal onClose={() => setShow(false)} show={show}/>
+                <div className="requestCards">
+                     <CommunityRequest cardTitle="Card 1" cardBody="Body of Card 1" cardDate="Date of Card 1"/>
+                     <CommunityRequest cardTitle="Card 2" cardBody="Body of Card 1" cardDate="Date of Card 2"/>
+                     <CommunityRequest cardTitle="Card 3" cardBody="Body of Card 1" cardDate="Date of Card 3"/>
+                     <CommunityRequest cardTitle="Card 4" cardBody="Body of Card 1" cardDate="Date of Card 4"/>
+                     <CommunityRequest cardTitle="Card 5" cardBody="Body of Card 1" cardDate="Date of Card 5"/>
+                     <CommunityRequest cardTitle="Card 6" cardBody="Body of Card 1" cardDate="Date of Card 6"/>
+                     <CommunityRequest cardTitle="Card 7" cardBody="Body of Card 1" cardDate="Date of Card 7"/>
+                     <CommunityRequest cardTitle="Card 7" cardBody="Body of Card 1" cardDate="Date of Card 8"/>
+                </div>
             </div>
         );
-    }
 }
 
 export default App;
